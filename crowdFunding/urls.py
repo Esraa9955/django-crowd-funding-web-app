314f5ccc66f775d5c611c4412559243069ba1a4a
EmanAmr1 <emanamr543@gmail.com>
2024-02-13 13:42:20 +0200

    Configure MEDIA_URL and MEDIA_ROOT for images uploads

--- crowdFunding/urls.py
+++ crowdFunding/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from .settings import *
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('project/',include('project.urls'))
-]
+]+static(MEDIA_URL,document_root=MEDIA_ROOT)
