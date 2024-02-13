2e94a7c2ad74ef1603369e91d268780855283cbd
EmanAmr1 <emanamr543@gmail.com>
2024-02-13 13:08:29 +0200

    connect with db postgreSQL

--- crowdFunding/settings.py
+++ crowdFunding/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'crowdFunding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "crowdfundingdb",
+        "HOST": "localhost",
+        "PORT":"5432",
+        "USER":"postgres",
+        "PASSWORD":"1234"
     }
 }
 
