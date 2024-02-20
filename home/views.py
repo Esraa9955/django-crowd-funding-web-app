4aede1c12a8efa52a4a3b03b5bfe913065c13ec0
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-02-21 00:26:39 +0200

    slider , featured projrct section and latest project section

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from project.models import *
+from django.shortcuts import get_object_or_404
 def home(request):
   projects = Project.objects.all()
   projectImage = ProjectImage.objects.all()
-  return render(request, 'home.html',{'projects':projects,'projectImage':projectImage})
+  highest_rated_projects = Project.objects.annotate(avg_rating=models.Avg('projectrating__rating')).order_by('-avg_rating')[:5]
+  latest_projects = Project.objects.all().order_by('-start_time')[:5]
+  context = {
+        'highest_rated_projects': highest_rated_projects,
+        'projects':projects,
+        'projectImage':projectImage,
+        'latest_projects':latest_projects
+    }
+  return render(request, 'home.html',context)
 
 def error_404(request, exception):
     data = {}
-    return render(request, 'home/404.html', data)
+    return render(request, '404.html', data)
