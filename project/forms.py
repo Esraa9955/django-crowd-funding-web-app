17892b100e9eeaeac0158eb012ec13a7f4174651
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-02-21 21:11:32 +0200

    Set the time

--- project/forms.py
+++ project/forms.py
@@ -1,10 +1,25 @@
 from django import forms
 from .models import *
+from django.core.exceptions import ValidationError
+from django.forms import inlineformset_factory
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'details', 'total_target', 'start_time', 'end_time', 'category','tags']
+        fields = ['title', 'details', 'total_target', 'start_time', 'end_time', 'donation_amount', 'is_featured', 'category', 'tags']
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get("start_time")
+        end_time = cleaned_data.get("end_time")
+
+        if start_time and end_time:
+            if end_time <= start_time:
+                raise ValidationError("End time must be after start time.")
+        return cleaned_data
+
+ImageFormSet = inlineformset_factory(Project, ProjectImage, fields=('image',))
 
 class ImageForm(forms.ModelForm):
     class Meta:
